70446f7c57c15cc8dec911c5066cfc71125e4d0b
Daniel Lacasse <daniel@nokee.dev>
2020-06-25 18:57:34 -0400

    Fix typo in repository name

--- gradle/plugins/docs-gradle-plugin/src/main/java/dev/nokee/docs/RepositoryUtils.java
+++ gradle/plugins/docs-gradle-plugin/src/main/java/dev/nokee/docs/RepositoryUtils.java
@@ -9,7 +9,7 @@ import static dev.nokee.docs.SecretValue.obtainSecret;
 public class RepositoryUtils {
 	public static Action<? super MavenArtifactRepository> nokeeDocumentation() {
 		return repository -> {
-			repository.setName("Nokee Documentation on S3");
+			repository.setName("nokee-documentation-on-s3");
 			repository.credentials(AwsCredentials.class, credentials -> {
 				credentials.setAccessKey(obtainSecret("AWS_ACCESS_KEY", "dev.nokee.aws.user"));
 				credentials.setSecretKey(obtainSecret("AWS_SECRET_KEY", "dev.nokee.aws.key"));
