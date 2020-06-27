848d8b260c9cd27501335d5b2d30da513e1d00f1
Daniel Lacasse <daniel@nokee.dev>
2020-06-27 12:02:38 -0400

    Update publish task to include deleted and new files

--- gradle/plugins/docs-gradle-plugin/src/main/java/dev/nokee/docs/tasks/PublishToGitHubPages.java
+++ gradle/plugins/docs-gradle-plugin/src/main/java/dev/nokee/docs/tasks/PublishToGitHubPages.java
@@ -4,7 +4,6 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.ResetCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
-import org.eclipse.jgit.transport.TagOpt;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.DirectoryProperty;
@@ -35,9 +34,8 @@ public abstract class PublishToGitHubPages extends DefaultTask {
 	private void doPublish() throws GitAPIException, IOException {
 		if (isGitRepository(getTemporaryDir())) {
 			try (Git git = Git.open(getTemporaryDir())) {
-				git.fetch().setTagOpt(TagOpt.NO_TAGS).call();
-				git.reset().setMode(ResetCommand.ResetType.HARD).setRef("refs/heads/gh-pages").call();
-				git.checkout().setName("gh-pages").call();
+				git.fetch().call();
+				git.reset().setMode(ResetCommand.ResetType.HARD).setRef("origin/gh-pages").call();
 			}
 		} else {
 			Git.cloneRepository().setDirectory(getTemporaryDir()).setURI("https://github.com/nokeedev/docs.nokee.dev.git").setBranch("gh-pages").setNoTags().call();
@@ -50,7 +48,7 @@ public abstract class PublishToGitHubPages extends DefaultTask {
 
 		try (Git git = Git.open(getTemporaryDir())) {
 			git.add().addFilepattern(".").call();
-			git.commit().setAuthor("nokeedevbot", "bot@nokee.dev").setMessage("Publish by nokeedevbot").call();
+			git.commit().setAuthor("nokeedevbot", "bot@nokee.dev").setMessage("Publish by nokeedevbot").setAll(true).call();
 			git.push().setCredentialsProvider(new UsernamePasswordCredentialsProvider(getGitHubKey().get(), getGitHubSecret().get())).call();
 		}
 	}
